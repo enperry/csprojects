cee720adae01c3a91a0a95543dd1bd896ec60402
enperry <andy2005@hotmail.co.kr>
2020-07-17 11:35:26 +0900

    new function

--- datavisualisation/graph.py
+++ datavisualisation/graph.py
@@ -3,6 +3,8 @@ from collections import Counter
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import geojson
+import parse as p
 
 MY_FILE = "/Users/kuro/Documents/GitHub/csprojects/datavisualisation/sample_sfpd_incident_all.csv"
 
@@ -55,8 +57,34 @@ def visualizeType():
     plt.savefig("Type.png")
     plt.clf()
 
+def createMap(dataFile):
+    geoMap = {"type" : "FeatureCollection"}
+    itemList = []
+
+    for index, line in enumerate(dataFile):
+        if(line["X"] == "0" or line["Y"] == "0"):
+            continue
+    
+    data = {}
+
+    data["type"] = "Feature"
+    data["id"] = index
+    data["properties"] = {"title" : line["Category"], "description" : line["Descript"], "date" : line["Date"]}
+    data["Geometry"] = {"type" : "Point", "coordinates" : (line["X"], line["Y"])}
+
+    itemList.append(data)
+
+    for point in itemList:
+        geoMap.setdefault("features", []).append(point)
+
+    with open("file_sf.geojson", "w") as f:
+        f.write(geojson.dumps(geoMap))
+
 def main():
     visualizeDays()
     visualizeType()
+    data = p.parse(p.MY_FILE, ",")
+    return createMap(data)
 
 main()
+
