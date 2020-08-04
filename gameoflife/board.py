a6152f99b2d6d72bff7158775146d1fe43820f88
enperry <andy2005@hotmail.co.kr>
2020-08-04 13:12:44 +0900

    yea

---
+++ gameoflife/board.py
@@ -0,0 +1,24 @@
+from cell import Cell
+from random import randint
+
+class Board:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.grid = [[Cell() for columnCells in range(self.columns)] for rowCells in range(self.rows)]
+        self.generateBoard()
+
+    def drawBoard(self):
+        print("\n"*10)
+        print("printing board...")
+        for row in self.grid:
+            for col in row:
+                print(col.getPrintCharacter(), end = "")
+        print()
+
+    def generateBoard(self):
+        for row in self.grid:
+            for col in row:
+                chanceNumber = randint(0, 2)
+                if(chanceNumber == 0):
+                    col.setLive()
---
+++ gameoflife/cell.py
@@ -0,0 +1,21 @@
+class Cell:
+    def __init__(self):
+        self.status = "DEAD"
+
+    def setDead(self): 
+        self.status = "DEAD"
+
+    def setLive(self):
+        self.status = "LIVE"
+    
+    def isAlive(self):
+        if(self.status == "LIVE"):
+            return True
+        else: 
+            return False
+    
+    def getPrintCharacter(self):
+        if(self.isAlive == True):
+            return u"\u2588"
+        else:
+            return " "
