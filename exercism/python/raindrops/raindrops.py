47e49758a35751ddf4f612293e1623b3c11124dc
enperry <andy2005@hotmail.co.kr>
2020-08-20 11:21:17 +0900

    thats right

---
+++ exercism/python/raindrops/raindrops.py
@@ -0,0 +1,14 @@
+def convert(number):
+    resultString = ""
+
+    if(number % 3 == 0):
+        resultString = resultString + "Pling"
+    if(number % 5 == 0):
+        resultString = resultString + "Plang"
+    if(number % 7 == 0):
+        resultString = resultString + "Plong"
+    
+    if(resultString == ""):
+        return number
+
+    return resultString
