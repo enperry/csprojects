d7a84727c5f9547e4a038e0bf62c30b36b46b648
enperry <andy2005@hotmail.co.kr>
2020-08-24 10:17:15 +0900

    exercism yea

---
+++ exercism/python/high-scores/high_scores.py
@@ -0,0 +1,12 @@
+def latest(scores):
+    return scores[len(scores) - 1]
+
+
+def personal_best(scores):
+    return max(scores)
+
+
+def personal_top_three(scores):
+    tempscores = scores
+    tempscores.sort(reverse = True)
+    return tempscores[:3]
