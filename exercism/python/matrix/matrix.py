bef94ebafa9e41ea2be5aab84140651026541b2c
enperry <andy2005@hotmail.co.kr>
2020-08-25 12:00:53 +0900

    thats right

---
+++ exercism/python/matrix/matrix.py
@@ -0,0 +1,10 @@
+class Matrix:
+    def __init__(self, matrix_string):
+        self.matrix = [[int(i) for i in line.split()] for line in matrix_string.split("\n")]
+
+    def row(self, index):
+        return self.matrix[index - 1]
+
+    def column(self, index):
+        return[x[index - 1] for x in self.matrix]
+
