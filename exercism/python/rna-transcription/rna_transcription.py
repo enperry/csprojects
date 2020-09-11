df4282783f0b9d513e7993f8d8df84231656ec9d
enperry <andy2005@hotmail.co.kr>
2020-09-11 13:39:31 +0900

    rna

--- exercism/python/rna-transcription/rna_transcription.py
+++ exercism/python/rna-transcription/rna_transcription.py
@@ -1,2 +1,14 @@
 def to_rna(dna_strand):
-    
+    for char in dna_strand:
+        if(char == "G"):
+            char = "C"
+        elif(char == "C"):
+            char = "G"
+        elif(char == "T"):
+            char = "A"
+        elif(char == "A"):
+            char = "U"
+        else:
+            char = " "
+
+    return dna_strand
