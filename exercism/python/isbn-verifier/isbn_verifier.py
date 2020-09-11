b9d7b0c00b91233e49783f616edc58638302b68e
enperry <andy2005@hotmail.co.kr>
2020-09-11 14:10:04 +0900

    isbn

--- exercism/python/isbn-verifier/isbn_verifier.py
+++ exercism/python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,13 @@
 def is_valid(isbn):
-    pass
+    isbn = list(isbn)
+    for char in isbn:
+        if(char == "-"):
+            isbn.remove(char)
+        if(char.index != 9 and char == "X"):
+            return "Invalid ISBN"
+
+    if(isbn[9] == "X"):
+        isbn[9] = 10
+    isbn = [int(i) for i in isbn]
+    
+    return (((isbn[0] * 10) + (isbn[1] * 9) + (isbn[2] * 8) + (isbn[3] * 7) + (isbn[4] * 6) + (isbn[5] * 5) + (isbn[6] * 4) + (isbn[7] * 3) + (isbn[8] * 2) + (isbn[9] * 1)) % 11)
