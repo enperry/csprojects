ec6d6c6d744c60f7416b2a8f19588d6c640fce68
enperry <andy2005@hotmail.co.kr>
2020-10-13 11:30:21 +0900

    matching brackets

---
+++ exercism/python/matching-brackets/matching_brackets.py
@@ -0,0 +1,12 @@
+def is_paired(input_string):
+    pairs = {'{': '}', '[': ']', '(': ')'}
+    stack = []
+
+    for char in input_string:
+        if(char in pairs):
+            stack.append(char)
+        elif char in pairs.values():
+            if(not stack or char != pairs[stack.pop()]):
+                return False
+
+    return not stack
